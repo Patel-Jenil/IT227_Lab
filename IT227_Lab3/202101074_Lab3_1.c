5307e01e9f1ff81bff620370412e2aa166ff49b0
Patel-Jenil <jenilpatel58@gmail.com>
2022-08-30 13:38:54 +0530

    Version 1.0.1 Lab3_1

--- IT227_Lab3/202101074_Lab3_1.c
+++ IT227_Lab3/202101074_Lab3_1.c
@@ -3,67 +3,63 @@
 // Jenil_Patel_202101074
 // array of function pointers (*fns[](int n))
 
-void fib(long n) {
-    long long A, A_1 = 1, A_2=1;
+void fib(unsigned long long n) {
+    unsigned long long A, A_1 = 1, A_2=1;
 	printf(":\n");
 	if (n <= 0) {
 		printf("0\n");
 		return;
 	}
-	if (1 <=n) {
+	if (1 <=n)
 		printf("1 ");
-	}
-	if (2 <=n ) {
+	if (2 <=n )
 		printf("1 ");
-	}
+		
 	for (long i=3; i<=n; ++i) {
 		A = A_1 + A_2;
-		printf("%lld ", A);
+		printf("%llu ", A);
 		A_1 = A_2;
 		A_2 = A;
 	}
 	printf("\n");
 }
 
-void fact(long n) {
+void fact(unsigned long long n) {
     if (n<0) {
 		printf("-1");
 		return;
 	}
-	long long ans = 1, temp = n; 
+	unsigned long long ans = 1; 
 	while (n--) {
-		ans *= temp;
+		ans *= (n+1);
 	}
-	printf("%lld\n" , ans);
+	printf("%llu\n" , ans);
 }
 
-void numlen(long n) {
-	if (n < 0) {
-		n *= -1;
-	}
-    int count = 0;
+void numlen(unsigned long long n) {
+    unsigned long count = 0;
     while (n>0 && ++count)
         n/=10;
-    printf("%d\n", count);
+    printf("%lu\n", count);
 }
 
-void stop(long n) {
+void stop(unsigned long long n) {
 	printf("\nAdios Amigo\n\n");
 	exit(0);
 }
 
 int main() {
-	void (*fns[4])(long n) = {fib, fact, numlen ,stop};
-	long int n;
+	void (*fns[4])(unsigned long long n) = {fib, fact, numlen ,stop};
+	unsigned long long n;
 	int choice;
     char *prefix[] = {"The Fibonacci series upto", "-->", "Length of the number [", ""};
     char *suffix[] = {"", "! = ", "] = ", ""};
 	while(1) {
 		printf("\n=====================================");
-		printf("\n1 for Fibonacci series upto n.\n");
-		printf("2 for Factorial\n");
-		printf("3 for Number-length\n");
-		printf("4 for Exit\n");
+		printf("\n1. Fibonacci series upto n.\n");
+		printf("2. Factorial\n");
+		printf("3. Number-length\n");
+		printf("4. Exit\n");
 		printf("Enter operation to perform: ");
 		scanf("%d", &choice);
 		if (choice<1 || choice>4)
@@ -72,11 +68,13 @@ int main() {
             if (choice == 4)
                 goto exitPoint;
 			printf("Enter the number: ");
-			scanf("%ld", &n);
-			printf("\n%s %ld %s",prefix[choice-1],n, suffix[choice-1]);
+			scanf("%llu", &n);
+			printf("\n%s %llu %s",prefix[choice-1],n, suffix[choice-1]);
             exitPoint:
 			fns[choice-1](n);
 		}
+		getchar();
+		getchar();
 	}
     return 0;
 }
